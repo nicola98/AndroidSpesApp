72a0bd3c1c411bc4ac37d59d2f708a22a53849a1
ilcanna <illnicolacannato@gmail.com>
2018-04-09 10:36:27 +0200

    aggiunta classi per visualizzazione cartelle

--- app/src/main/java/com/example/corsista/androidspesapp/data/test.java
+++
@@ -1,8 +0,0 @@
-package com.example.corsista.androidspesapp.data;
-
-/**
- * Created by Corsista on 09/04/2018.
- */
-
-public class test {
-}
--- app/src/main/java/com/example/corsista/androidspesapp/logic/test.java
+++
@@ -1,8 +0,0 @@
-package com.example.corsista.androidspesapp.logic;
-
-/**
- * Created by Corsista on 09/04/2018.
- */
-
-public class test {
-}

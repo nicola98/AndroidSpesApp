65735b344e9f5f3189b3dff4b3f047c296692b29
Deathgun998 <davi.fuma@hotmail.com>
2018-04-18 12:11:25 +0200

    profile.png aggiunta nav bar aggiunta con profilo che usa un toast in attesa della creazione della activity profile

--- app/src/main/java/com/example/corsista/androidspesapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/corsista/androidspesapp/ui/activity/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,9 +35,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
 
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         int id = item.getItemId();
+
+        switch(id){
+            case R.id.profile:
+                Toast.makeText(this, "profile works", Toast.LENGTH_SHORT).show();
+                return true;
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
 }
